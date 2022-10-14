6862485c513f55ce770518af58755b3c0c324ef9
kamly77 <bakoko2326@mail.ru>
2022-10-14 13:04:02 +0300

    first commit

---
+++ Projects/2practica/2practica/Program.cs
@@ -0,0 +1,27 @@
+﻿Random rand = new Random();
+int MyNum;
+int secret = rand.Next(1,101);
+int count = 1;
+Console.WriteLine("Компьютер загадал число от 0 до 100.Попробуйте отгадать");
+while(true)
+{
+    MyNum = int.Parse(Console.ReadLine());
+    if (MyNum == secret)
+    {
+        Console.WriteLine("Угадал,бро");
+        break;   
+    }
+    else
+    {
+        if(MyNum > secret)
+        {
+            Console.WriteLine("Меньше,бро");
+        }
+        else
+        {
+            Console.WriteLine("Больше,бро");
+        }
+    }
+}
+
+Console.ReadKey();
